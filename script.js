f86ef6f89a954ed55b116d95cd2375a777d2a200
vetrivk <vetrivvs07@gmail.com>
2024-01-05 22:46:53 +0530

    update file

--- script.js
+++ script.js
@@ -1,48 +1,23 @@
-(function () {
-  const customerImage = document.quertSelector("#customer-img");
-  const customerName = document.quertSelector("#customer-name");
-  const customerText = document.quertSelector("#customer-text");
-
-  const btn = document.querySelectorAll(".btn");
-  let index = 0;
-  const customer = [];
-
-  function customer(img, name, text) {
-    this.img = img;
-    this.name = name;
-    this.text = TextDecoder;
-  }
-
-  function createcustomer(img, name, text) {
-    let Img = `./img/${img}.jpg`;
-    let customer = new customer(img, name, text);
-
-    customer.push(customer);
-  }
-
-  createcustomer(
-    0,
-    "Prakash",
-    "Hello Our Service is best than other accusamus! Facere assumenda, beatae in libero reiciendis ipsa laudantium voluptatibus ? "
-  );
-  createcustomer(
-    1,
-    "Durga",
-    "Hello Our Service is best than other accusamus! Facere assumenda, beatae in libero reiciendis ipsa laudantium voluptatibus ? "
-  );
-  createcustomer(
-    2,
-    "Vetri",
-    "Hello Our Service is best than other accusamus! Facere assumenda, beatae in libero reiciendis ipsa laudantium voluptatibus ? "
-  );
-  createcustomer(
-    3,
-    "Siva",
-    "Hello Our Service is best than other accusamus! Facere assumenda, beatae in libero reiciendis ipsa laudantium voluptatibus ? "
-  );
-  createcustomer(
-    4,
-    "Aravind",
-    "Hello Our Service is best than other accusamus! Facere assumenda, beatae in libero reiciendis ipsa laudantium voluptatibus ? "
-  );
-})();
+const data = [
+  {
+    id: 1,
+    names: "Vetri.Coder",
+    role: "webDeveloper",
+    img: "/JavaProject/image/3 jpg.jpg",
+    text: "I am Vetri and I come from Madurai City. I finished my Bsc Conputer Science Degree 2018.Lately I have been getting into IT, especfically learning not developer program. ",
+  },
+  {
+    id: 1,
+    names: "Vetri.Coder",
+    role: "webDeveloper",
+    img: "/JavaProject/image/4 jpg.jpg",
+    text: "I am Vetri and I come from Madurai City. I finished my Bsc Conputer Science Degree 2018.Lately I have been getting into IT, especfically learning not developer program. ",
+  },
+  {
+    id: 1,
+    names: "Vetri.Coder",
+    role: "webDeveloper",
+    img: "/JavaProject/image/2 jpg.jpg",
+    text: "I am Vetri and I come from Madurai City. I finished my Bsc Conputer Science Degree 2018.Lately I have been getting into IT, especfically learning not developer program. ",
+  },
+];
